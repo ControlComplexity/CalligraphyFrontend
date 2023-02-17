85e65719a154b85f686d6017e1c9e72d2318dbb8
wangcao <LVQian280@>
2023-02-17 13:13:38 +0800

    增加艺术家和朝代表

---
+++ src/assets/data/artist.js
@@ -0,0 +1,94 @@
+let data = [
+    {
+        id: 0,
+        name: '夏商周',
+        url: ''
+    },
+    {
+        id: 1,
+        name: '秦朝',
+        url: ''
+    },
+    {
+        id: 2,
+        name: '汉朝',
+        url: ''
+    },
+    {
+        id: 3,
+        name: '魏晋南北朝',
+        url: ''
+    },
+    {
+        id: 4,
+        name: '隋朝',
+        url: ''
+    },,
+    {
+        id: 5,
+        name: '唐朝',
+        url: ''
+    },
+    {
+        id: 6,
+        name: '宋朝',
+        url: ''
+    },
+    {
+        id: 7,
+        name: '元朝',
+        url: ''
+    },
+    {
+        id: 8,
+        name: '明朝',
+        url: ''
+    },
+    {
+        id: 9,
+        name: '清朝',
+        url: ''
+    },
+    {
+        id: 10,
+        name: '近现代&当代',
+        url: ''
+    }
+];
+
+let famousArtists =[
+    { 
+        id:1,
+        name: 王羲之,
+        dynasty: 魏晋南北朝,
+        url: ''
+    },
+    { 
+        id:2,
+        name: 颜真卿,
+        dynasty: 唐朝,
+        url: ''
+    },  { 
+        id:3,
+        name: 田英章,
+        dynasty: 当代,
+        url: ''
+    },
+    { 
+        id:4,
+        name: 欧阳询,
+        dynasty: 唐朝,
+        url: ''
+    },{ 
+        id:5,
+        name: 卢中南,
+        dynasty: 当代,
+        url: ''
+    },
+    { 
+        id:6,
+        name: 苏轼,
+        dynasty: 宋朝,
+        url: ''
+    }
+]
